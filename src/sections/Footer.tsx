ff1c697750044c83c96122a5cb4d5d5bba0c6d6f
Mohd Amaan <mhdamaan790@gmail.com>
2025-07-21 19:15:40 +0530

    footer section completed

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,6 @@
+import logoImage from "@/assets/images/logo.png";
+import Image from "next/image";
+
 const footerLinks = [
     { href: "#", label: "Contact" },
     { href: "#", label: "Privacy Policy" },
@@ -5,5 +8,37 @@ const footerLinks = [
 ];
 
 export default function Footer() {
-    return <div>Footer</div>;
+    return (
+        <section className="py-16">
+            <div className="container">
+                <div className="flex flex-col md:flex-row items-center md:justify-between gap-6">
+                    <div>
+                        <div className="flex items-center justify-start gap-2">
+                            <Image
+                                src={logoImage}
+                                alt="Strata Logo"
+                                className="h-8 md:h-10 w-auto"
+                            />
+                            <h1 className="font-semibold text-2xl md:text-3xl tracking-wide">
+                                Strata
+                            </h1>
+                        </div>
+                    </div>
+                    <div>
+                        <nav className="flex gap-6">
+                            {footerLinks.map((link) => (
+                                <a
+                                    href={link.href}
+                                    key={link.label}
+                                    className="text-white/50 text-sm"
+                                >
+                                    {link.label}
+                                </a>
+                            ))}
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
